4a5ff5f887b8b8a3b92a912214c207a28b2cb720
Raghavendra Kukkadapu <raghavendrakukkadapu12@gmail.com>
2025-07-19 11:56:40 +0530

    Update Search Box.jsx

--- src/Search Box.jsx
+++ src/Search Box.jsx
@@ -7,7 +7,7 @@ export default function SearchBox({updateInfo}){
     let [city , setCity] = useState("");
     let [error , setError] = useState(false);
     const API_URL = "https://api.openweathermap.org/data/2.5/weather"
-    const API_KEY = "YOUR_API_KEY" // we have removed our api key add your own Api key to perform project
+    const API_KEY = import.meta.env.VITE_APP_WEATHER_API_KEY;
 
     let getWeatherInfo = async() =>{
        try{
